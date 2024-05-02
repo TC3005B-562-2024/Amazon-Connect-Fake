268c84c0ce68fe3c875b6a0d5b604d01d1406249
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-05-01 22:23:50 -0600

    CORS and fixed mysql connection eror

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from data_fakeada import FakeInfo
 
 
 app = Flask(__name__)
-CORS(app, origins='*')
+CORS(app, origins="*")
 db = mysql.connector.connect(
     host="",
     user="",
@@ -17,6 +17,7 @@ db = mysql.connector.connect(
     database="",
 )
 fake_data = FakeInfo()
+
 
 @app.errorhandler(400)
 def bad_request_error(error):
@@ -26,6 +27,7 @@ def bad_request_error(error):
     error_message = f"Error {error.code}: {error.name}"
     return jsonify({"error": error_message}), 400
 
+
 @app.errorhandler(404)
 def not_found_error(error):
     """
@@ -34,38 +36,45 @@ def not_found_error(error):
     error_message = f"Error {error.code}: {error.name}"
     return jsonify({"error": error_message}), 404
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
     """
     Returns a hello world message
     """
-    return 'Hello from Flask!'
+    return "Hello from Flask!"
+
 
 @app.route("/fake/info", methods=["POST"])
 def metrics_data():
     """
     Returns fake metrics based on the alert id that is requested
     """
-    data = request.json
-    cursor = db.cursor()
-    cursor.execute(
-        "SELECT resource, is_solved FROM alert WHERE identifier = %s",
-        (data["identifier"],),
-    )
-    result = cursor.fetchone()
-    cursor.close()
-    # Get the resource column from the result
-    # example: "routing-profile:123456"
-    if not result:
-        return jsonify({"error": "Alert not found or resource is null"}), 404
-    resource_arn = result[0].split(":")[0]
-    is_solved = True if result[1] == 1 else False
-
     try:
-        data_to_return = fake_data.fake_info(resource_arn, is_solved)
-    except ValueError:
-        return jsonify({"error": "Invalid type"}), 400
-    return jsonify(data_to_return)
+        data = request.json
+        db.reconnect()
+        cursor = db.cursor()
+        cursor.execute(
+            "SELECT resource, is_solved FROM alert WHERE identifier = %s",
+            (data["identifier"],),
+        )
+        result = cursor.fetchone()
+        cursor.close()
+        # Get the resource column from the result
+        # example: "routing-profile:123456"
+        if not result:
+            return jsonify({"error": "Alert not found or resource is null"}), 404
+        resource_arn = result[0].split(":")[0]
+        is_solved = True if result[1] == 1 else False
+
+        try:
+            data_to_return = fake_data.fake_info(resource_arn, is_solved)
+        except ValueError:
+            return jsonify({"error": "Invalid type"}), 400
+        return jsonify(data_to_return)
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == "__main__":
